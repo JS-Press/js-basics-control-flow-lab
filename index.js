361d73c85aad09bee3401c7422b2a8ffc4b04862
Jarad Solomon <jaradsolomon@gmail.com>
2021-05-22 15:44:46 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+let feet;
+
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  
+  if (feet === 199) 
+      return 'This one is on me!';
+  if (feet >= 2000 && feet < 2500) 
+      return 'I will gladly take your thirty bucks.';
+  if (feet >= 2501) 
+      return 'No can do.';
+  
+
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+return (city == 'NYC' ? "Ok, sounds good." : "No go.");
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(charm){
   // Write your code here!
+switch (charm) {
+  case 'generous' :  
+    return 'Thank you so much.';
+  case 'not as generous' : 
+    return 'Thank you.';
+  default:
+    return 'Bye.';
+ 
+}
+
 }
